f43a6fb5bf3c944cec9c6a7b443eaff58a932bac
André Rouél <andre.rouel@googlemail.com>
2012-07-15 20:12:50 +0200

    Added an workaround to test OnlineUserAgentStringParserImpl with a logger at the lowest log level setting (to get all branches)

---
+++ modules/uadetector-core/src/test/java/net/sf/uadetector/util/FinalStaticFieldValueChangerTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright 2012 André Rouél
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package net.sf.uadetector.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FinalStaticFieldValueChangerTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(FinalStaticFieldValueChangerTest.class);
+
+	@Test
+	public void test() throws SecurityException, NoSuchFieldException, Exception {
+		FinalStaticFieldValueChanger.setFinalStatic(Boolean.class.getField("FALSE"), true);
+		Assert.assertEquals(true, false); // unmaintainable code ;-)
+		LOG.info(String.format("%s: Everything is %s", this.getClass().getSimpleName(), false)); // "Everything is true"
+	}
+
+}
---
+++ modules/uadetector-core/src/test/java/net/sf/uadetector/util/LowestLogLevelTestLogger.java
@@ -0,0 +1,328 @@
+/*******************************************************************************
+ * Copyright 2012 André Rouél
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package net.sf.uadetector.util;
+
+import org.slf4j.Logger;
+import org.slf4j.Marker;
+
+public final class LowestLogLevelTestLogger implements Logger {
+
+	@Override
+	public void debug(final Marker marker, final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final Marker marker, final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final Marker marker, final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final Marker marker, final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final Marker marker, final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void debug(final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final Marker marker, final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final Marker marker, final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final Marker marker, final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final Marker marker, final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final Marker marker, final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void error(final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public String getName() {
+		return this.getClass().getName();
+	}
+
+	@Override
+	public void info(final Marker marker, final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final Marker marker, final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final Marker marker, final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final Marker marker, final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final Marker marker, final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void info(final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public boolean isDebugEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isDebugEnabled(final Marker marker) {
+		return false;
+	}
+
+	@Override
+	public boolean isErrorEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isErrorEnabled(final Marker marker) {
+		return false;
+	}
+
+	@Override
+	public boolean isInfoEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isInfoEnabled(final Marker marker) {
+		return false;
+	}
+
+	@Override
+	public boolean isTraceEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isTraceEnabled(final Marker marker) {
+		return false;
+	}
+
+	@Override
+	public boolean isWarnEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isWarnEnabled(final Marker marker) {
+		return false;
+	}
+
+	@Override
+	public void trace(final Marker marker, final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final Marker marker, final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final Marker marker, final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final Marker marker, final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final Marker marker, final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void trace(final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final Marker marker, final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final Marker marker, final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final Marker marker, final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final Marker marker, final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final Marker marker, final String msg, final Throwable t) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final String msg) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final String format, final Object arg) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final String format, final Object arg1, final Object arg2) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final String format, final Object[] argArray) {
+		// do nothing
+	}
+
+	@Override
+	public void warn(final String msg, final Throwable t) {
+		// do nothing
+	}
+
+}

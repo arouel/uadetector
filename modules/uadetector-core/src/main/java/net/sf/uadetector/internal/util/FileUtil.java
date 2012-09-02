2de0176814a3951d673e0742d8b065941b39dff8
André Rouél <andre.rouel@googlemail.com>
2012-09-02 20:01:16 +0200

    Added utility class for files to check the emptiness

---
+++ modules/uadetector-core/src/main/java/net/sf/uadetector/internal/util/FileUtil.java
@@ -0,0 +1,48 @@
+package net.sf.uadetector.internal.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+/**
+ * This class is intended to provide file utility functions.
+ * 
+ * @author André Rouél
+ */
+public final class FileUtil {
+
+	/**
+	 * Checks if the given file is empty.
+	 * 
+	 * @param file
+	 *            the file that could be empty
+	 * @return {@code true} when the file is accessible and empty otherwise {@code false}
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	public static boolean isEmpty(final File file, final Charset charset) throws IOException {
+		boolean empty = false;
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+			final String line = reader.readLine();
+			empty = line == null;
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+		}
+		return empty;
+	}
+
+	/**
+	 * <strong>Attention:</strong> This class is not intended to create objects from it.
+	 */
+	private FileUtil() {
+		// This class is not intended to create objects from it.
+	}
+
+}
---
+++ modules/uadetector-core/src/test/java/net/sf/uadetector/internal/util/FileUtilTest.java
@@ -0,0 +1,66 @@
+package net.sf.uadetector.internal.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import junit.framework.Assert;
+import net.sf.uadetector.datastore.DataStore;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ BufferedReader.class, FileUtil.class })
+public class FileUtilTest {
+
+	/**
+	 * The character set to read the contents of an URL
+	 */
+	private static final Charset CHARSET = DataStore.DEFAULT_CHARSET;
+
+	/**
+	 * URL to retrieve the version of the UAS data
+	 */
+	private static final URL VERSION_URL = FileUtilTest.class.getClassLoader().getResource("uas_newer.version");
+
+	@Test
+	public void isEmpty_withEmptyFile() throws Exception {
+		final File tempEmptyFile = File.createTempFile("testfile", ".tmp");
+		tempEmptyFile.deleteOnExit();
+		final boolean isEmpty = FileUtil.isEmpty(tempEmptyFile, CHARSET);
+		PowerMock.verifyAll();
+		Assert.assertTrue(isEmpty);
+		tempEmptyFile.delete();
+	}
+
+	@Test
+	public void isEmpty_withFilledFile() throws Exception {
+		final File tempEmptyFile = File.createTempFile("testfile", ".tmp");
+		tempEmptyFile.deleteOnExit();
+		final FileOutputStream writer = new FileOutputStream(tempEmptyFile);
+		writer.write("test".getBytes());
+		writer.close();
+		final boolean isEmpty = FileUtil.isEmpty(tempEmptyFile, CHARSET);
+		PowerMock.verifyAll();
+		Assert.assertFalse(isEmpty);
+		tempEmptyFile.delete();
+	}
+
+	@Test(expected = IOException.class)
+	public void testNullCheckBeforeClosing() throws Exception {
+		PowerMock.expectNiceNew(BufferedReader.class, EasyMock.anyObject(Reader.class)).andThrow(new IOException());
+		PowerMock.replayAll();
+		FileUtil.isEmpty(new File(VERSION_URL.toURI()), CHARSET);
+		PowerMock.verifyAll();
+	}
+
+}

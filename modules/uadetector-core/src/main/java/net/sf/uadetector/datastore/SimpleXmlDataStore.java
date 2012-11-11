f0e4936493517aaa24b982eecfe1638e8d8ed540
André Rouél <andre.rouel@googlemail.com>
2012-11-11 17:21:38 +0100

    Added a simple implementation of a (non-refreshable) `DataStore`

---
+++ modules/uadetector-core/src/main/java/net/sf/uadetector/datastore/SimpleXmlDataStore.java
@@ -0,0 +1,54 @@
+package net.sf.uadetector.datastore;
+
+import java.net.URL;
+
+import net.sf.uadetector.datareader.DataReader;
+import net.sf.uadetector.datareader.XmlDataReader;
+import net.sf.uadetector.internal.data.Data;
+
+/**
+ * This is the simplest implementation of a {@link DataStore}. It initialize the store by reading the <em>UAS data</em>
+ * via the passed URL and store it only in the Java heap space.
+ * 
+ * <p>
+ * The given resource must have a valid XML format with UTF-8 charset that validates against specified schema under <a
+ * href="http://user-agent-string.info/rpc/uasxmldata.dtd">http://user-agent-string.info/rpc/uasxmldata.dtd</a>.
+ * 
+ * @author André Rouél
+ */
+public final class SimpleXmlDataStore extends AbstractDataStore implements DataStore {
+
+	/**
+	 * The default data reader to read in <em>UAS data</em> in XML format
+	 */
+	private static final DataReader DEFAULT_DATA_READER = new XmlDataReader();
+
+	/**
+	 * Constructs an {@code SimpleXmlDataStore} by reading <em>UAS data</em> by the specified default URL
+	 * {@link DataStore#DEFAULT_DATA_URL} (in XML format).
+	 * 
+	 * @param dataUrl
+	 *            URL to <em>UAS data</em>
+	 * @param versionUrl
+	 *            URL to version information about the given <em>UAS data</em>
+	 */
+	public SimpleXmlDataStore(final URL dataUrl, final URL versionUrl) {
+		super(DEFAULT_DATA_READER, dataUrl, versionUrl, DEFAULT_CHARSET);
+	}
+
+	/**
+	 * Constructs an {@code SimpleXmlDataStore} by reading <em>UAS data</em> by the specified default URL
+	 * {@link DataStore#DEFAULT_DATA_URL} (in XML format).
+	 * 
+	 * @param dataUrl
+	 *            URL to <em>UAS data</em>
+	 * @param versionUrl
+	 *            URL to version information about the given <em>UAS data</em>
+	 * @param fallback
+	 *            <em>UAS data</em> as fallback in case the data on the specified resource can not be read correctly
+	 */
+	public SimpleXmlDataStore(final URL dataUrl, final URL versionUrl, final Data fallback) {
+		super(DEFAULT_DATA_READER, dataUrl, versionUrl, DEFAULT_CHARSET, fallback);
+	}
+
+}
---
+++ modules/uadetector-core/src/test/java/net/sf/uadetector/datastore/SimpleXmlDataStoreTest.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright 2012 André Rouél
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package net.sf.uadetector.datastore;
+
+import java.net.URL;
+
+import net.sf.uadetector.internal.data.Data;
+import net.sf.uadetector.internal.data.DataBlueprint;
+import net.sf.uadetector.internal.util.UrlUtil;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleXmlDataStoreTest {
+
+	/**
+	 * URL to retrieve the UAS data as XML
+	 */
+	private static final URL DATA_URL = AbstractDataStoreTest2.class.getClassLoader().getResource("uas_newer.xml");
+
+	/**
+	 * {@link URL} that is not available
+	 */
+	private static final URL UNREACHABLE_URL = UrlUtil.build("http://unreachable.local/");
+
+	/**
+	 * URL to retrieve the version of the UAS data
+	 */
+	private static final URL VERSION_URL = AbstractDataStoreTest2.class.getClassLoader().getResource("uas_newer.version");
+
+	@Test
+	public void construct_successful() {
+		final SimpleXmlDataStore store = new SimpleXmlDataStore(DATA_URL, VERSION_URL);
+		Assert.assertTrue(!store.getData().getVersion().isEmpty());
+		Assert.assertNotNull(store.getDataReader());
+		Assert.assertNotNull(store.getDataUrl());
+		Assert.assertNotNull(store.getVersionUrl());
+	}
+
+	@Test
+	public void readData_failsAndReturnsFallbackData() {
+		final String version = "fallback-data-version";
+		final Data fallback = DataBlueprint.buildEmptyTestData(version);
+		final SimpleXmlDataStore store = new SimpleXmlDataStore(UNREACHABLE_URL, UNREACHABLE_URL, fallback);
+		Assert.assertEquals(store.getData().getVersion(), version);
+		Assert.assertNotNull(store.getDataReader());
+		Assert.assertNotNull(store.getDataUrl());
+		Assert.assertNotNull(store.getVersionUrl());
+	}
+
+}

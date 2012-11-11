86dfcfe720a94c37eac1392e98a16ccb202f16a1
André Rouél <andre.rouel@googlemail.com>
2012-11-11 18:03:49 +0100

    Refactored unit test to avoid different results depending on the order that the test methods run by using JUnit's TemporaryFolder class (belongs to issue 9)

--- modules/uadetector-core/src/test/java/net/sf/uadetector/datastore/CachingXmlDataStoreTest2.java
+++ modules/uadetector-core/src/test/java/net/sf/uadetector/datastore/CachingXmlDataStoreTest2.java
@@ -7,15 +7,14 @@ import java.net.URL;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
-import java.util.Random;
 
 import net.sf.uadetector.datareader.XmlDataReaderTest;
 import net.sf.uadetector.parser.UpdatingUserAgentStringParserImpl;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class CachingXmlDataStoreTest2 {
 
@@ -54,16 +53,6 @@ public class CachingXmlDataStoreTest2 {
 	 */
 	private static final Charset CHARSET = DataStore.DEFAULT_CHARSET;
 
-	/**
-	 * Temporary file to cache <em>UAS data</em>
-	 */
-	private static File temp;
-
-	/**
-	 * The default temporary-file directory
-	 */
-	private static final String CACHE_DIR = System.getProperty("java.io.tmpdir");
-
 	private static String readFile(final File file) throws IOException {
 		final FileInputStream stream = new FileInputStream(file);
 		try {
@@ -76,53 +65,40 @@ public class CachingXmlDataStoreTest2 {
 		}
 	}
 
-	@BeforeClass
-	public static void setUp_class() throws IOException {
-		// create temp file
-		temp = new File(CACHE_DIR, "uas_temp_" + new Random().nextLong() + ".tmp");
-		temp.createNewFile();
-		temp.deleteOnExit();
-		Assert.assertEquals("", readFile(temp));
-	}
-
-	@AfterClass
-	public static void tearDown_class() throws IOException {
-		// delete temp file
-		temp.delete();
-	}
+	/**
+	 * Temporary folder to cache <em>UAS data</em> in a file. Created files in this folder are guaranteed to be deleted
+	 * when the test method finishes (whether it passes or fails).
+	 */
+	@Rule
+	public final TemporaryFolder folder = new TemporaryFolder();
 
 	@Test
-	public void _1_create_withEmptyCache() throws IOException, InterruptedException {
-		// create caching data store
-		final CachingXmlDataStore store = CachingXmlDataStore.createCachingXmlDataStore(temp, DATA_URL_OLDER, VERSION_URL_OLDER, CHARSET);
+	public void testDifferentCacheFileAndUpdateStates() throws IOException, InterruptedException {
+		// setup a temporary file
+		final File cache = folder.newFile();
+
+		// create caching data store without a cache file
+		final CachingXmlDataStore store1 = CachingXmlDataStore.createCachingXmlDataStore(cache, DATA_URL_OLDER, VERSION_URL_OLDER, CHARSET);
+		final UpdatingUserAgentStringParserImpl parser1 = new UpdatingUserAgentStringParserImpl(store1);
+		parser1.parse("test");
+		final String readIn1 = readFile(cache);
+		Assert.assertTrue(readIn1.contains(DATA_VERSION_OLDER));
+		Assert.assertTrue(readIn1.length() >= 721915);
+
+		// create caching data store with filled cache and available update
+		final CachingXmlDataStore store2 = CachingXmlDataStore.createCachingXmlDataStore(cache, DATA_URL_NEWER, VERSION_URL_NEWER, CHARSET);
+		final UpdatingUserAgentStringParserImpl parser2 = new UpdatingUserAgentStringParserImpl(store2);
+		parser2.parse("test");
+		final String readIn2 = readFile(cache);
+		Assert.assertTrue(readIn2.contains(DATA_VERSION_NEWER));
+		Assert.assertTrue(readIn2.length() >= 721915);
+
+		// create caching data store with filled cache and without an available update
+		final CachingXmlDataStore store = CachingXmlDataStore.createCachingXmlDataStore(cache, DATA_URL_NEWER, VERSION_URL_NEWER, CHARSET);
 		final UpdatingUserAgentStringParserImpl parser = new UpdatingUserAgentStringParserImpl(store);
 		parser.parse("test");
 
-		final String readIn = readFile(temp);
-		Assert.assertTrue(readIn.contains(DATA_VERSION_OLDER));
-		Assert.assertTrue(readIn.length() >= 721915);
-	}
-
-	@Test
-	public void _2_create_withFilledCache_andAvailableUpdate() throws IOException, InterruptedException {
-		// create caching data store
-		final CachingXmlDataStore store = CachingXmlDataStore.createCachingXmlDataStore(temp, DATA_URL_NEWER, VERSION_URL_NEWER, CHARSET);
-		final UpdatingUserAgentStringParserImpl parser = new UpdatingUserAgentStringParserImpl(store);
-		parser.parse("test");
-
-		final String readIn = readFile(temp);
-		Assert.assertTrue(readIn.contains(DATA_VERSION_NEWER));
-		Assert.assertTrue(readIn.length() >= 721915);
-	}
-
-	@Test
-	public void _3_create_withFilledCache_andNoAvailableUpdate() throws IOException, InterruptedException {
-		// create caching data store
-		final CachingXmlDataStore store = CachingXmlDataStore.createCachingXmlDataStore(temp, DATA_URL_NEWER, VERSION_URL_NEWER, CHARSET);
-		final UpdatingUserAgentStringParserImpl parser = new UpdatingUserAgentStringParserImpl(store);
-		parser.parse("test");
-
-		final String readIn = readFile(temp);
+		final String readIn = readFile(cache);
 		Assert.assertTrue(readIn.contains(DATA_VERSION_NEWER));
 		Assert.assertTrue(readIn.length() >= 721915);
 	}

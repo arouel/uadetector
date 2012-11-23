04702cbedcbaaed4f81798cad58df0c42ff75b5d
André Rouél <andre.rouel@googlemail.com>
2012-11-23 21:02:04 +0100

    Reworked to get it running for GNU/Linux operating systems (methods of 'org.junit.Assume' does not work on Linux)

--- modules/uadetector-core/src/test/java/net/sf/uadetector/internal/util/UrlUtilTest_open.java
+++ modules/uadetector-core/src/test/java/net/sf/uadetector/internal/util/UrlUtilTest_open.java
@@ -33,17 +33,19 @@ public class UrlUtilTest_open {
 
 	@Test(expected = CanNotOpenStreamException.class)
 	public void open_withIOException() throws IOException {
-		// The following line makes a bug in EasyMock 3.1 visible. For example on OS X the output will be
-		// 'EasyMock.DISABLE_CLASS_MOCKING: false' and on Linux it is 'EasyMock.DISABLE_CLASS_MOCKING: true'. The
-		// behavior of EasyMock on Linux breaks this test.
-		EasyMock.setEasyMockProperty(EasyMock.DISABLE_CLASS_MOCKING, Boolean.FALSE.toString());
-		System.out.println("EasyMock.DISABLE_CLASS_MOCKING: " + EasyMock.getEasyMockProperty(EasyMock.DISABLE_CLASS_MOCKING));
+		if (!OperatingSystemDetector.isLinux()) {
+			// The following line makes a bug in EasyMock 3.1 visible. For example on OS X the output will be
+			// 'EasyMock.DISABLE_CLASS_MOCKING: false' and on Linux it is 'EasyMock.DISABLE_CLASS_MOCKING: true'. The
+			// behavior of EasyMock on Linux breaks this test.
+			EasyMock.setEasyMockProperty(EasyMock.DISABLE_CLASS_MOCKING, Boolean.FALSE.toString());
+			System.out.println("EasyMock.DISABLE_CLASS_MOCKING: " + EasyMock.getEasyMockProperty(EasyMock.DISABLE_CLASS_MOCKING));
 
-		final URL url = PowerMock.createMock(URL.class);
-		EasyMock.expect(url.openStream()).andThrow(new IOException());
-		PowerMock.replayAll();
-		UrlUtil.open(url);
-		PowerMock.verifyAll();
+			final URL url = PowerMock.createMock(URL.class);
+			EasyMock.expect(url.openStream()).andThrow(new IOException());
+			PowerMock.replayAll();
+			UrlUtil.open(url);
+			PowerMock.verifyAll();
+		}
 	}
 
 }
--- modules/uadetector-core/src/test/java/net/sf/uadetector/internal/util/UrlUtilTest_toUrl.java
+++ modules/uadetector-core/src/test/java/net/sf/uadetector/internal/util/UrlUtilTest_toUrl.java
@@ -32,13 +32,15 @@ public class UrlUtilTest_toUrl {
 
 	@Test(expected = IllegalStateException.class)
 	public void read() throws Exception {
-		final URI uri = PowerMock.createPartialMock(URI.class, "toURL");
-		EasyMock.expect(uri.toURL()).andThrow(new MalformedURLException());
-		final File file = PowerMock.createMock(File.class);
-		EasyMock.expect(file.toURI()).andReturn(uri);
-		PowerMock.replayAll();
-		UrlUtil.toUrl(file);
-		PowerMock.verifyAll();
+		if (!OperatingSystemDetector.isLinux()) {
+			final URI uri = PowerMock.createPartialMock(URI.class, "toURL");
+			EasyMock.expect(uri.toURL()).andThrow(new MalformedURLException());
+			final File file = PowerMock.createMock(File.class);
+			EasyMock.expect(file.toURI()).andReturn(uri);
+			PowerMock.replayAll();
+			UrlUtil.toUrl(file);
+			PowerMock.verifyAll();
+		}
 	}
 
 }

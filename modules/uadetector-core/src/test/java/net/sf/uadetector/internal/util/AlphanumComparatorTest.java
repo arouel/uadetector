9e15372b634131ad948fe050cba3cd4068237ee7
Thomas Draier <tdraier@jahia.com>
2016-09-29 14:34:08 +0200

    #133 : replaced alphanum by other implementation

--- modules/uadetector-core/src/test/java/net/sf/uadetector/internal/util/AlphanumComparatorTest.java
+++ modules/uadetector-core/src/test/java/net/sf/uadetector/internal/util/AlphanumComparatorTest.java
@@ -29,61 +29,61 @@ public class AlphanumComparatorTest {
 	public void compare_0_0() {
 		final String charA = "0";
 		final String charB = "0";
-		assertThat(new AlphanumComparator().compare(charA, charB)).isEqualTo(0);
+		assertThat(new NaturalOrderComparator().compare(charA, charB)).isEqualTo(0);
 	}
 
 	@Test
 	public void compare_0_0000() {
 		final String charA = "0";
 		final String charB = "0000";
-		assertThat(new AlphanumComparator().compare(charA, charB)).isEqualTo(-3);
-		assertThat(new AlphanumComparator().compare(charB, charA)).isEqualTo(3);
+		assertThat(new NaturalOrderComparator().compare(charA, charB)).isLessThan(0);
+		assertThat(new NaturalOrderComparator().compare(charB, charA)).isGreaterThan(0);
 	}
 
 	@Test
 	public void compare_00001_00001() {
 		final String charA = "00001";
 		final String charB = "00001";
-		assertThat(new AlphanumComparator().compare(charA, charB)).isEqualTo(0);
+		assertThat(new NaturalOrderComparator().compare(charA, charB)).isEqualTo(0);
 	}
 
 	@Test
 	public void compare_02_2() {
 		final String charA = "02";
 		final String charB = "2";
-		assertThat(new AlphanumComparator().compare(charA, charB)).isEqualTo(1);
-		assertThat(new AlphanumComparator().compare(charB, charA)).isEqualTo(-1);
+		assertThat(new NaturalOrderComparator().compare(charA, charB)).isGreaterThan(0);
+		assertThat(new NaturalOrderComparator().compare(charB, charA)).isLessThan(0);
 	}
 
 	@Test
 	public void compare_02_20() {
 		final String charA = "02";
 		final String charB = "20";
-		assertThat(new AlphanumComparator().compare(charA, charB)).isEqualTo(-2);
-		assertThat(new AlphanumComparator().compare(charB, charA)).isEqualTo(2);
+		assertThat(new NaturalOrderComparator().compare(charA, charB)).isLessThan(0);
+		assertThat(new NaturalOrderComparator().compare(charB, charA)).isGreaterThan(0);
 	}
 
 	@Test
 	public void compare_0A_0000A() {
 		final String charA = "0A";
 		final String charB = "0000A";
-		assertThat(new AlphanumComparator().compare(charA, charB)).isEqualTo(-3);
-		assertThat(new AlphanumComparator().compare(charB, charA)).isEqualTo(3);
+		assertThat(new NaturalOrderComparator().compare(charA, charB)).isLessThan(0);
+		assertThat(new NaturalOrderComparator().compare(charB, charA)).isGreaterThan(0);
 	}
 
 	@Test
 	public void compare_differentFlags() {
 		final String version1 = "0";
 		final String version2 = "00";
-		assertThat(new AlphanumComparator().compare(version1, version2)).isEqualTo(-1);
-		assertThat(new AlphanumComparator().compare(version2, version1)).isEqualTo(1);
+		assertThat(new NaturalOrderComparator().compare(version1, version2)).isLessThan(0);
+		assertThat(new NaturalOrderComparator().compare(version2, version1)).isGreaterThan(0);
 	}
 
 	@Test
 	public void compare_identical() {
 		final String version1 = "000-12";
 		final String version2 = "000-12";
-		assertThat(new AlphanumComparator().compare(version1, version2)).isEqualTo(0);
+		assertThat(new NaturalOrderComparator().compare(version1, version2)).isEqualTo(0);
 	}
 
 	@Test
@@ -112,56 +112,33 @@ public class AlphanumComparatorTest {
 		final List<String> scrambled = Arrays.asList(src.toArray(new String[] {}));
 		Collections.copy(scrambled, src);
 		Collections.shuffle(scrambled);
-		Collections.sort(scrambled, new AlphanumComparator());
+		Collections.sort(scrambled, new NaturalOrderComparator());
 
 		assertThat(scrambled).isEqualTo(src);
 	}
 
 	@Test
 	public void compare_null_1() {
-		new AlphanumComparator().compare(null, null);
+		new NaturalOrderComparator().compare(null, null);
 	}
 
 	@Test
 	public void compare_null_2() {
 		final String version = "1";
-		new AlphanumComparator().compare(null, version);
+		new NaturalOrderComparator().compare(null, version);
 	}
 
 	@Test
 	public void compare_null_3() {
 		final String version = "2";
-		new AlphanumComparator().compare(version, null);
+		new NaturalOrderComparator().compare(version, null);
 	}
 
 	@Test
 	public void compare_same() {
 		final String version = "4";
-		assertThat(new AlphanumComparator().compare(version, version)).isEqualTo(0);
+		assertThat(new NaturalOrderComparator().compare(version, version)).isEqualTo(0);
 	}
 
-	@Test
-	public void compareDigits_001_xyz() {
-		final String charA = "001";
-		final String charB = "xyz";
-		assertThat(AlphanumComparator.compareDigits(charA, charB)).isEqualTo(-72);
-		assertThat(AlphanumComparator.compareDigits(charB, charA)).isEqualTo(72);
-	}
-
-	@Test
-	public void compareDigits_1_00() {
-		final String charA = "1";
-		final String charB = "00";
-		assertThat(AlphanumComparator.compareDigits(charA, charB)).isEqualTo(-1);
-		assertThat(AlphanumComparator.compareDigits(charB, charA)).isEqualTo(1);
-	}
-
-	@Test
-	public void compareDigits_abc_xyz() {
-		final String charA = "abc";
-		final String charB = "xyz";
-		assertThat(AlphanumComparator.compareDigits(charA, charB)).isEqualTo(-23);
-		assertThat(AlphanumComparator.compareDigits(charB, charA)).isEqualTo(23);
-	}
 
 }
